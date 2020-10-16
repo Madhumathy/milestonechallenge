3b4aa05024e1eecbb320ca3a445d13e8fdf93270
Madhumathy <34366346+Madhumathy@users.noreply.github.com>
2020-10-16 21:14:29 +0530

    updated Calc file with return statement

--- src/main/java/javaApp/Calc.java
+++ src/main/java/javaApp/Calc.java
@@ -3,21 +3,21 @@ public class Calc {
     public int add(int a, int b) {
     //insert your code here
 
-
+return 0;
     }
     public int subtract(int a, int b) {
     //insert your code here
 
-
+return 0;
     }
     public int multiply(int a, int b) {
     //insert your code here
-
+return 0;
 
     }
     public int divide(int a, int b) {
     //insert your code here
 
-
+return 0;
     }
 }  
