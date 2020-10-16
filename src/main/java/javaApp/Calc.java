6952a6b907f9b86d3cd25a4536ade751a203808a
Madhumathy <34366346+Madhumathy@users.noreply.github.com>
2020-10-16 21:27:27 +0530

    Update Calc.java

--- src/main/java/javaApp/Calc.java
+++ src/main/java/javaApp/Calc.java
@@ -2,22 +2,22 @@ package javaApp;
 public class Calc {
     public int add(int a, int b) {
     //insert your code here
-
+return a + b;
 
     }
     public int subtract(int a, int b) {
     //insert your code here
 
-
+return a - b;
     }
     public int multiply(int a, int b) {
     //insert your code here
 
-
+return a * b;
     }
     public int divide(int a, int b) {
     //insert your code here
 
-
+return a / b;
     }
 }  
